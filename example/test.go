5f4a6cda6728b243365ef724b957347eaffc5dc2
forthxu <forthxu@gmail.com>
2017-11-20 09:40:28 +0800

    update 增加错误处理

--- example/test.go
+++ example/test.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	//"io/ioutil"
 	"os/exec"
 
-	"github.com/forthxu/qbot"
+	//"github.com/forthxu/qbot"
 )
 
 func execCommand(path string) {
@@ -14,17 +14,73 @@ func execCommand(path string) {
 		//fmt.Println("Error: ", err)
 	}
 }
+/*
+function hash(b, i) {
+	for (var a = [], s = 0; s < i.length; s++)
+		a[s % 4] ^= i.charCodeAt(s);
+
+	console.log(a)
+	var j = ["EC", "OK"], d = [];
+	d[0] = b >> 24 & 255 ^ j[0].charCodeAt(0);
+	d[1] = b >> 16 & 255 ^ j[0].charCodeAt(1);
+	d[2] = b >> 8 & 255 ^ j[1].charCodeAt(0);
+	d[3] = b & 255 ^ j[1].charCodeAt(1);
+	console.log(d)
+
+	j = [];
+	for (s = 0; s < 8; s++)
+		j[s] = s % 2 == 0 ? a[s >> 1] : d[s >> 1];
+	console.log(jj)
+
+	a = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+	d = "";
+	for (s = 0; s < j.length; s++)
+		d += a[j[s] >> 4 & 15], d += a[j[s] & 15];
+	return d;
+}
+hash(472767085,"")
+*/
+func hash(b int, i string) string {
+	a := [4]int{}
+	for s := 0; s < len(i); s++{
+		a[s%4] ^= int(i[s])
+	}
+	
+	j := []string{"EC", "OK"}
+	d := []int{b >> 24 & 255 ^ int(j[0][0]), b >> 16 & 255 ^ int(j[0][1]),b >> 8 & 255 ^ int(j[1][0]),b & 255 ^ int(j[1][1])}
+
+	jj := [8]int{}
+	for s := 0; s < 8; s++{
+		if s%2==0 {
+			jj[s] = a[s >> 1]
+		}else{
+			jj[s] = d[s >> 1]
+		}
+	}	
+	
+	aa := [16]string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"}
+	dd := ""
+	for s := 0; s < len(jj); s++{
+		dd += aa[jj[s] >> 4 & 15]
+		dd += aa[jj[s] & 15]
+	}
+	
+	return dd;
+}
 
 func main() {
-	r, _ := qbot.New()
+
+	hash(472767085,"")
+
+	fmt.Println('a')
+
+	/*
 	r.OnQRChange(func(r *qbot.Robot, qrdata []byte) {
 		if err := ioutil.WriteFile("v.png", qrdata, 0666); err == nil {
 			execCommand("v.png")
 		}
 	})
 	r.OnMessage(func(r *qbot.Robot, message *qbot.Message) {
-		fmt.Println(message)
-
 		switch message.PollType {
 		case "message":
 			r.SendToBuddy(message.FromUin, message.Content+"\r\n\t--qbot")
@@ -35,4 +91,5 @@ func main() {
 		}
 	})
 	r.Run()
+	*/
 }
--- robot.go
+++ robot.go
@@ -282,8 +282,18 @@ func (r *Robot) pollMessage() {
 			code := ParseMessage(r, data)
 			if code == 103 {
 				log.Println("登陆失败。请先在浏览器访问http://w.qq.com/扫码登录，然后退出。重新启动程序")
-				//break
+				//这个是由于Smart QQ多点登录，后端校验失败。需要手动进入官网，检查是否能正常接收消息。如果可以的话点击[设置]->[退出登录]后查看是否恢复正常
+				break
 				r.getOnline()
+			}else if code == 108 || code == 114 || code == 121 {
+				log.Println(code, "掉线")
+				break
+			}else if code == 122 || code == 100001{
+				log.Println(code, "掉线")
+				break
+			}else if code == 100006{
+				log.Println(code, "群编号有问题")
+				break
 			}
 			if code == 100001 {
 				break
